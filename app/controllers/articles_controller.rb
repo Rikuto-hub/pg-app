c011f082aedd0706c903774a076589abcd37327a
中村陸人 <>
2020-09-20 15:50:11 +0900

    ArticleModelを作成

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,36 @@
+class ArticlesController < ApplicationController
+    def index
+        @language = Language.find(params[:language_id])
+        @articles = @language.articles
+    end
+    def show
+        @article = Article.find(params[:id])
+    end
+    def new
+        language = Language.find(params[:language_id])
+        @article = language.articles.build
+    end
+    def create
+        language = Language.find(params[:language_id])
+        @article = language.articles.build(article_params)
+        if @article.save
+            redirect_to language_articles_path, notice: 'save'
+        else
+            flash.now[:error] = 'miss'
+            render :new
+        end
+    end
+    def edit
+        
+    end
+    def update
+        
+    end
+    def destroy
+        
+    end
+    private
+    def article_params
+        params.require(:article).permit(:title, :error, :content, :memo, :number)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_19_153226) do
+ActiveRecord::Schema.define(version: 2020_09_19_172123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.bigint "language_id", null: false
+    t.string "title", null: false
+    t.text "error", null: false
+    t.text "content", null: false
+    t.text "memo", null: false
+    t.integer "number", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["language_id"], name: "index_articles_on_language_id"
+  end
 
   create_table "languages", force: :cascade do |t|
     t.bigint "user_id", null: false
