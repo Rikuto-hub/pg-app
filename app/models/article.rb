10b92688bf0191389891cc49a6edc4b0990fc8f7
中村陸人 <>
2020-10-02 10:28:01 +0900

    戻るボタンとflashの追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many_attached :article_imgs
     belongs_to :language
+
+    validates :title, presence: true
+    validates :error, presence: true
+    validates :number, presence: true
 end
