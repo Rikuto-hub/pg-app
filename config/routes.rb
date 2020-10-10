e2c806e3cbc36f7c28f320d3f2cd11376b0d2d12
中村陸人 <>
2020-10-10 13:31:33 +0900

    serviceの登録フォームを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "languages#index"
   resource :profile, only:[:show, :edit, :update]
+  resource :service, only:[:edit, :update]
   resources :languages do
     resources :articles 
   end
